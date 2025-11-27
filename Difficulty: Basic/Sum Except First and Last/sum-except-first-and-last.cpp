4869388e90f97346da5f4b756c7a38ea3a61d0f1
Rahul Das <rd2432007@gmail.com>
2025-11-27 13:07:30 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Sum Except First and Last/sum-except-first-and-last.cpp
@@ -0,0 +1,17 @@
+class Solution {
+  public:
+    /*You are required to complete this method*/
+    int sumExceptFirstLast(vector<int>& arr) {
+        // Your code here
+        int n=arr.size();
+        int sum=0;
+        
+        if(n<=2) return 0;
+        
+        for(int i=1;i<n-1;i++){
+            sum+=arr[i];
+        }
+        
+        return sum;
+    }
+};

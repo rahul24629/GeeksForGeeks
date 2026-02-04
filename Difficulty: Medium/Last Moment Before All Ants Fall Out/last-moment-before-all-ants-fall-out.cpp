46577259d4cb64c176d4b725a8a1d6238a3e4a15
Rahul Das <rd2432007@gmail.com>
2026-02-04 22:00:27 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Last Moment Before All Ants Fall Out/last-moment-before-all-ants-fall-out.cpp
@@ -0,0 +1,17 @@
+class Solution {
+  public:
+    int getLastMoment(int n, vector<int>& left, vector<int>& right) {
+        // code here
+        int ans=0;
+        
+        for(int x:left){
+            ans=max(ans,x);
+        }
+        
+        for(int x:right){
+            ans=max(ans,n-x);
+        }
+        
+        return ans;
+    }
+};

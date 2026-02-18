e3272923c7462177f034ef2c83ef2658df1f33ae
Rahul Das <rd2432007@gmail.com>
2026-02-19 00:53:27 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Missing Element in Range/missing-element-in-range.cpp
@@ -0,0 +1,14 @@
+class Solution {
+  public:
+    vector<int> missinRange(vector<int>& arr, int low, int high) {
+        // code here
+        unordered_set<int> s(arr.begin(),arr.end());
+        vector<int> ans;
+        for(int i=low;i<=high;i++){
+            if(s.find(i)==s.end()){
+                ans.push_back(i);
+            }
+        }
+        return ans;
+    }
+};

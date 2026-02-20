86796d07db3bace57c0d497e31549747f1cb642c
Rahul Das <rd2432007@gmail.com>
2026-02-20 19:40:03 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Coin Change (Minimum Coins)/coin-change-minimum-coins.cpp
@@ -0,0 +1,22 @@
+class Solution {
+  public:
+    int minCoins(vector<int> &coins, int sum) {
+        // code here
+        int n=coins.size();
+        const int INF = 1e9;
+        vector<vector<int>> dp(n+1,vector<int>(sum+1,INF));
+        for(int i=0;i<=n;i++){
+            dp[i][0]=0;
+        }
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=sum;j++){
+                if(coins[i-1]<=j){
+                    dp[i][j]=min(dp[i][j-coins[i-1]]+1, dp[i-1][j]);
+                }else{
+                    dp[i][j]=dp[i-1][j];
+                }
+            }
+        }
+        return dp[n][sum]==INF?-1:dp[n][sum];
+    }
+};

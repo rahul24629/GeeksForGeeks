5ac92e776b3a619475bb46e932117d6458bc1385
Rahul Das <rd2432007@gmail.com>
2026-02-23 01:09:56 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Count number of hops/count-number-of-hops.cpp
@@ -0,0 +1,16 @@
+
+class Solution {
+  public:
+    // Function to count the number of ways in which frog can reach the top.
+    int countWays(int n) {
+
+        // your code here
+        vector<int> dp(n+1);
+        dp[0]=1;
+        dp[1]=1;
+        for(int i=2;i<=n;i++){
+            dp[i]=dp[i-1]+ dp[i-2] + dp[i-3];
+        }
+        return dp[n];
+    }
+};

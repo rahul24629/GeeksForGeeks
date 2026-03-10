8483dc0f43f44df03d4558335ca8f7e99430d49b
Rahul Das <rd2432007@gmail.com>
2026-03-11 00:48:44 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Parenthesis Checker/parenthesis-checker.cpp
@@ -0,0 +1,22 @@
+class Solution {
+  public:
+    bool isBalanced(string& s) {
+        // code here
+        stack<char> st;
+        for(int i=0;i<s.size();i++){
+            if(s[i]=='[' || s[i]=='{' || s[i]=='(') st.push(s[i]);
+            else{
+                if(st.empty()) return false;
+                
+                char top=st.top();
+                st.pop();
+                
+                if(top=='(' && s[i]!=')' || top=='{' && s[i]!='}' || top=='[' && s[i]!=']') return false;
+                
+            }
+                  
+        }
+        return st.empty();
+        
+    }
+};

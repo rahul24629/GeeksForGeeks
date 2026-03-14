f764aabde5bd0cc1f4fcacdef20ee1093e88ce9e
Rahul Das <rd2432007@gmail.com>
2026-03-15 00:41:22 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Count the number of ways to tile the floor of size n x m using 1 x m size tiles/count-the-number-of-ways-to-tile-the-floor-of-size-n-x-m-using-1-x-m-size-tiles.cpp
@@ -0,0 +1,16 @@
+
+class Solution {
+  public:
+    int countWays(int n, int m) {
+        // Code here.
+        const int MOD = 1000000007;
+        vector<long long> dp(n+1,0);
+        dp[0]=1;
+        for(int i=1;i<=n;i++){
+            if(i<m) dp[i]=1;
+            else if(i==m) dp[i]=2;
+            else dp[i]=(dp[i-1]+dp[i-m])%MOD;
+        }
+        return dp[n];
+    }
+};

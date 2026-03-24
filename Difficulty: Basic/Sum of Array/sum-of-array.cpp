50f5ff8d966bf559eb34be034a7eb49c7e318977
Rahul Das <rd2432007@gmail.com>
2026-03-24 19:38:21 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Sum of Array/sum-of-array.cpp
@@ -0,0 +1,13 @@
+// User function template for C++
+class Solution {
+  public:
+    // Function to return sum of elements
+    int arraySum(vector<int>& arr) {
+        // code here
+        int sum=0;
+        for(int x:arr){
+            sum+=x;
+        }
+        return sum;
+    }
+};

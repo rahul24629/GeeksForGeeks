0f9d7d56422c229e8c0f7f78a1e009264f039160
Rahul Das <rd2432007@gmail.com>
2026-04-17 19:50:32 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Anagram Palindrome/anagram-palindrome.cpp
+++ Difficulty: Basic/Anagram Palindrome/anagram-palindrome.cpp
@@ -1,20 +1,25 @@
-int isPossible(string S) {
-    // your code here
-    int n=S.size();
-    vector<int> arr(26,0);
-    for(int i=0;i<S.size();i++){
-        int idx=S[i]-'a';
-        arr[idx]++;
-    }
-    int count=0;
-    for(int i=0;i<26;i++){
-        if(arr[i]%2!=0){
-            count++;
+class Solution {
+  public:
+    bool canFormPalindrome(string &s) {
+        // code here
+        int freq[26]={0};
+        
+        for(char c:s){
+            int idx=c-'a';
+            freq[idx]++;
+        }
+        int count=0;
+        for(int i=0;i<26;i++){
+            if(freq[i]%2!=0){
+                count++;
+            }
+        }
+        if(s.size()%2==0 && count==0){
+            return true;
+        }else if(s.size()%2==1 && count==1){
+            return true;
+        }else{
+            return false;
         }
     }
-    
-    if(n%2==0 && count==0) return 1;
-    else if(n%2!=0 && count==1) return 1;
-    else return 0;
-    
-}
+};

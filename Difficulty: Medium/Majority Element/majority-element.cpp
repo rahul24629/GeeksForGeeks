dbd0f1d08b4e3e1c963f41b53548b90425c1e62c
Rahul Das <rd2432007@gmail.com>
2026-08-01 15:01:45 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Majority Element/majority-element.cpp
+++ Difficulty: Medium/Majority Element/majority-element.cpp
@@ -1,34 +1,32 @@
 class Solution {
   public:
-    int count(vector<int>&arr,int num,int low, int high){
-        int count=0;
-        for(int i=low;i<=high;i++){
-            if(arr[i]==num) count++;
-        }
-        return count;
-    }
-    int majRec(vector<int>&arr , int start,int end){
-        if(start==end) return arr[start];
-        
-        int mid=start+(end-start)/2;
-        
-        int left=majRec(arr,start,mid);
-        int right=majRec(arr,mid+1,end);
-        
-        //find Occurence 
-        int leftCount=count(arr,left,start,end);
-        int rightCount=count(arr,right,start,end);
-        
-        if(leftCount>rightCount) return left;
-        else return right;
-        
-        
-    }
     int majorityElement(vector<int>& arr) {
         // code here
-        int candidate = majRec(arr, 0, arr.size() - 1);
-        int cnt = count(arr, candidate, 0, arr.size() - 1);
-        if (cnt > arr.size() / 2) return candidate;
-        else return -1; 
+        int el;
+        int count=0;
+        
+        for(int i=0;i<arr.size();i++){
+            if(count==0){
+                el=arr[i];
+                count=1;
+            }else if(el==arr[i]){
+                count++;
+            }else{
+                count--;
+            }
+        }
+        
+        int cnt1=0;
+        
+        for(int i=0;i<arr.size();i++){
+            if(arr[i]==el){
+                cnt1++;
+            }
+        }
+        
+        if(cnt1>arr.size()/2){
+            return el;
+        }
+        else return -1;
     }
 };

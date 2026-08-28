72f5a0d1c26a831e43f7322cdd9ef44b5773bdf0
Rahul Das <rd2432007@gmail.com>
2026-08-29 00:05:36 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Majority Element - More Than n/3/majority-element-more-than-n3.cpp
+++ Difficulty: Medium/Majority Element - More Than n/3/majority-element-more-than-n3.cpp
@@ -1,21 +1,51 @@
 class Solution {
   public:
     vector<int> findMajority(vector<int>& arr) {
-        // Code here
-        map<int,int> s;
+        // code here
+        int count1=0;
+        int el1=INT_MAX;
+        
+        int count2=0;
+        int el2=INT_MAX;
+        
         for(int i=0;i<arr.size();i++){
-            if(s.count(arr[i])){
-                s[arr[i]]++;
+            if(count1==0 && arr[i]!=el2){
+                count1=1;
+                el1=arr[i];
+            }else if(count2==0 && arr[i]!=el1){
+                count2=1;
+                el2=arr[i];
+            }else if(arr[i]== el1){
+                count1++;
+            }else if(arr[i]==el2){
+                count2++;
             }else{
-                s[arr[i]]=1;
+                count1--;
+                count2--;
             }
         }
-        vector<int> ans;
-        for(pair<int,int>p:s){
-            if(p.second> arr.size()/3){
-                ans.push_back(p.first);
-            }
+    
+    //manuel check
+    count1=0;
+    count2=0;
+    
+    vector<int> ans;
+    
+    for(int i=0;i<arr.size();i++){
+        if(arr[i]==el1){
+            count1++;
+        }else if(arr[i]==el2){
+            count2++;
         }
-        return ans;
+    }
+    
+    int minimal = (int)arr.size()/3 +1;
+    
+    if(count1>=minimal) ans.push_back(el1);
+    if(count2>=minimal) ans.push_back(el2);
+    
+    sort(ans.begin(),ans.end());
+    
+    return ans;
     }
 };

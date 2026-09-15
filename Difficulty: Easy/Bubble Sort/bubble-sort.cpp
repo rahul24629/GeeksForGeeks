ae36ff81d89dbe41d9506869695acd203b3eff9b
Rahul Das <rd2432007@gmail.com>
2026-09-15 21:40:39 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Bubble Sort/bubble-sort.cpp
+++ Difficulty: Easy/Bubble Sort/bubble-sort.cpp
@@ -1,19 +1,13 @@
 class Solution {
   public:
     void bubbleSort(vector<int>& arr) {
-        int n = arr.size();
-        
-        for(int i = 0; i < n - 1; i++){
-            bool swapped = false;
-            
-            for(int j = 0; j < n - i - 1; j++){
-                if(arr[j] > arr[j+1]){
-                    swap(arr[j], arr[j+1]);
-                    swapped = true;
-                }
+        // code here
+        int n=arr.size();
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n-i-1;j++){
+                if(arr[j]>arr[j+1]) swap(arr[j],arr[j+1]);
             }
-            
-            if(!swapped) break;
         }
+        
     }
 };

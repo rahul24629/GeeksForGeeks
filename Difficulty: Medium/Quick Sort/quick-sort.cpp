1ba7a0d0069e447db4936f94de46e3563a5cce78
Rahul Das <rd2432007@gmail.com>
2026-09-17 11:57:41 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Quick Sort/quick-sort.cpp
+++ Difficulty: Medium/Quick Sort/quick-sort.cpp
@@ -2,26 +2,23 @@ class Solution {
   public:
     void quickSort(vector<int>& arr, int low, int high) {
         // code here
-        if(low>= high) return;
+        if(low>=high) return;
         
-        int pivotIdx= partition(arr,low,high);
+        int pivotIdx=partition(arr,low,high);
         
         quickSort(arr,low,pivotIdx-1);
         quickSort(arr,pivotIdx+1,high);
-        
     }
 
     int partition(vector<int>& arr, int low, int high) {
         // code here
-        
         int i=low-1;
-        
-        int pivot=arr[high];
+        int pivotIdx=arr[high];
         
         for(int j=low;j<high;j++){
-            if(arr[j]<pivot){
+            if(arr[j]<pivotIdx){
                 i++;
-                swap(arr[i],arr[j]);
+                swap(arr[j], arr[i]);
             }
         }
         swap(arr[i+1],arr[high]);
